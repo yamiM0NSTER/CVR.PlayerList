7f5c07271a5df843dbea71c8dc9f6ba3970e0b42
yamiM0NSTER <jomex50@gmail.com>
2022-08-08 11:54:12 +0200

    Add Simple Events for callbacks

---
+++ Events/QuickMenu.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CVR.PlayerListMod.Events {
+  internal class QuickMenu {
+    public static event Action OnQuickMenuOpen;
+    public static event Action OnQuickMenuClose;
+
+    public static void OnToggleOpen(bool open) {
+      if(open) {
+        OnQuickMenuOpen?.Invoke();
+      } else {
+        OnQuickMenuClose?.Invoke();
+      }
+    }
+    public static void OnOpen() => OnQuickMenuOpen?.Invoke();
+    public static void OnClose() => OnQuickMenuClose?.Invoke();
+  }
+}
