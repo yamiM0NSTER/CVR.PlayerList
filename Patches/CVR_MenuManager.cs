7a78c0860df89bf45e34f986b4930667aaf173c2
yamiM0NSTER <jomex50@gmail.com>
2022-08-08 11:54:36 +0200

    Add CVR_MenuManager patches

---
+++ Patches/CVR_MenuManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace CVR.PlayerListMod.Patches {
+  internal class CVR_MenuManager {
+    public static void Patch() {
+      var harmonyInstance = ModMain.Instance.HarmonyInstance;
+      harmonyInstance.Patch(
+                typeof(ABI_RC.Core.InteractionSystem.CVR_MenuManager).GetMethod(nameof(ABI_RC.Core.InteractionSystem.CVR_MenuManager.ToggleQuickMenu)),
+                null,
+                new HarmonyLib.HarmonyMethod(typeof(CVR_MenuManager).GetMethod(nameof(ToggleQuickMenu_Postfix), BindingFlags.Static | BindingFlags.NonPublic))
+            );
+
+      harmonyInstance.Patch(
+                typeof(ABI_RC.Core.InteractionSystem.CVR_MenuManager).GetMethod("Start", BindingFlags.Instance | BindingFlags.NonPublic),
+                null,
+                new HarmonyLib.HarmonyMethod(typeof(CVR_MenuManager).GetMethod(nameof(Start_Postfix), BindingFlags.Static | BindingFlags.NonPublic))
+            );
+    }
+
+    static void ToggleQuickMenu_Postfix(bool __0) {
+      Events.QuickMenu.OnToggleOpen(__0);
+    }
+
+    static void Start_Postfix(ref ABI_RC.Core.InteractionSystem.CVR_MenuManager __instance) {
+      __instance.quickMenu.gameObject.AddComponent<Components.PlayerListLoader>();
+    }
+  }
+}
