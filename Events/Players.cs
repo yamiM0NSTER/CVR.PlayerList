dfb25a7af5d157c8d37daaa6cb328060624c6f3a
yamiM0NSTER <jomex50@gmail.com>
2022-08-08 13:38:40 +0200

    Add Player Events

---
+++ Events/Players.cs
@@ -0,0 +1,21 @@
+﻿using ABI_RC.Core.Player;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CVR.PlayerListMod.Events {
+  internal class Players {
+    public static event Action<CVRPlayerEntity> OnRemotePlayerJoin;
+    public static event Action<string> OnRemotePlayerLeave;
+
+    public static void RemotePlayerJoin(CVRPlayerEntity player) {
+      OnRemotePlayerJoin?.Invoke(player);
+    }
+
+    public static void RemotePlayerLeave(string playerId) {
+      OnRemotePlayerLeave?.Invoke(playerId);
+    }
+  }
+}
