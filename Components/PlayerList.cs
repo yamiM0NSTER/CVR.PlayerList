e9224e2fb3f7ed33bdc55c2d9c014911aad9dd44
yamiM0NSTER <jomex50@gmail.com>
2022-08-08 11:56:24 +0200

    Add PlayerList and PlayerListLoader Components

---
+++ Components/PlayerList.cs
@@ -0,0 +1,50 @@
+﻿using MelonLoader;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace CVR.PlayerListMod.Components {
+  internal class PlayerList : MonoBehaviour {
+    MelonLogger.Instance _logger;
+    GameObject _gameObject;
+    RectTransform _rectTransform;
+
+    void Awake() {
+      _logger = ModMain.Instance.Logger;
+      _gameObject = gameObject;
+      _rectTransform = GetComponent<RectTransform>();
+      SubscribeToEvents();
+    }
+    void Start() {
+      _rectTransform.localScale = new Vector3(0.0004f, 0.0004f, 1f);
+      _rectTransform.anchoredPosition = new Vector2(0.69f, 0);
+      _gameObject.layer = 5; // UI Layer
+      _gameObject.SetActive(false);
+    }
+
+    void SubscribeToEvents() {
+      Events.QuickMenu.OnQuickMenuClose += OnQuickMenuClose;
+      Events.QuickMenu.OnQuickMenuOpen += OnQuickMenuOpen;
+    }
+
+    void UnsubscribeFromEvents() {
+      Events.QuickMenu.OnQuickMenuClose -= OnQuickMenuClose;
+      Events.QuickMenu.OnQuickMenuOpen -= OnQuickMenuOpen;
+    }
+
+    void OnDestroy() {
+      UnsubscribeFromEvents();
+    }
+
+    void OnQuickMenuOpen() {
+      _gameObject.SetActive(true);
+    }
+
+    void OnQuickMenuClose() {
+      _gameObject.SetActive(false);
+    }
+  }
+}
---
+++ Components/PlayerListLoader.cs
@@ -0,0 +1,27 @@
+﻿using CVR.PlayerListMod.Utilities;
+using MelonLoader;
+using System;
+using UnityEngine;
+
+namespace CVR.PlayerListMod.Components {
+  public class PlayerListLoader : MonoBehaviour {
+    MelonLogger.Instance _logger;
+    GameObject _playerList = null;
+
+    private void Awake() {
+      _logger = ModMain.Instance.Logger;
+    }
+
+    private void Start() {
+      StartCoroutine(AssetBundleLoader.Co_LoadBundle(Resources.Resources.playerlistmod, new Action<AssetBundle>(Test)));
+    }
+
+    const string assetPath = "Assets/Prefabs/PlayerListMod.prefab";
+    void Test(AssetBundle assetBundle) {
+      var prefab = assetBundle.LoadAsset<GameObject>(assetPath);
+      _playerList = UnityEngine.Object.Instantiate(prefab, gameObject.transform);
+      _playerList.AddComponent<PlayerList>();
+      Destroy(this);
+    }
+  }
+}
