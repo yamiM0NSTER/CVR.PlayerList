de73c2038d11a224f52a9d05bd6241d4bc8f3080
yamiM0NSTER <jomex50@gmail.com>
2022-08-08 13:38:52 +0200

    Refactoring

--- Patches/CVR_MenuManager.cs
+++ Patches/CVR_MenuManager.cs
@@ -1,32 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Reflection;
+﻿using System.Reflection;
+using MenuManager = ABI_RC.Core.InteractionSystem.CVR_MenuManager;
 
 namespace CVR.PlayerListMod.Patches {
   internal class CVR_MenuManager {
     public static void Patch() {
       var harmonyInstance = ModMain.Instance.HarmonyInstance;
       harmonyInstance.Patch(
-                typeof(ABI_RC.Core.InteractionSystem.CVR_MenuManager).GetMethod(nameof(ABI_RC.Core.InteractionSystem.CVR_MenuManager.ToggleQuickMenu)),
+                typeof(MenuManager).GetMethod(nameof(MenuManager.ToggleQuickMenu)),
                 null,
                 new HarmonyLib.HarmonyMethod(typeof(CVR_MenuManager).GetMethod(nameof(ToggleQuickMenu_Postfix), BindingFlags.Static | BindingFlags.NonPublic))
             );
 
       harmonyInstance.Patch(
-                typeof(ABI_RC.Core.InteractionSystem.CVR_MenuManager).GetMethod("Start", BindingFlags.Instance | BindingFlags.NonPublic),
+                typeof(MenuManager).GetMethod("Start", BindingFlags.Instance | BindingFlags.NonPublic),
                 null,
                 new HarmonyLib.HarmonyMethod(typeof(CVR_MenuManager).GetMethod(nameof(Start_Postfix), BindingFlags.Static | BindingFlags.NonPublic))
             );
     }
 
-    static void ToggleQuickMenu_Postfix(bool __0) {
-      Events.QuickMenu.OnToggleOpen(__0);
+    static void ToggleQuickMenu_Postfix(bool show) {
+      Events.QuickMenu.OnToggleOpen(show);
     }
 
-    static void Start_Postfix(ref ABI_RC.Core.InteractionSystem.CVR_MenuManager __instance) {
+    static void Start_Postfix(ref MenuManager __instance) {
       __instance.quickMenu.gameObject.AddComponent<Components.PlayerListLoader>();
     }
   }
