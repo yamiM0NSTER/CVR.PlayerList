ca00e52cab11995b8b049d4e50c2d63102699e6f
yamiM0NSTER <jomex50@gmail.com>
2022-08-08 11:56:05 +0200

    Add AssetBundleLoader

---
+++ Utilities/AssetBundleLoader.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using CVR.PlayerListMod.Resources;
+
+namespace CVR.PlayerListMod.Utilities {
+  internal class AssetBundleLoader {
+    public static AssetBundle LoadBundle(byte[] data, bool dontUnload = true) {
+      AssetBundle assetBundle = AssetBundle.LoadFromMemory(data);
+      if(dontUnload)
+        assetBundle.hideFlags |= HideFlags.DontUnloadUnusedAsset;
+      return assetBundle;
+    }
+
+    public static IEnumerator Co_LoadBundle(byte[] data, Action<AssetBundle> callback, bool dontUnload = true) {
+      var bundleCreateRequest = AssetBundle.LoadFromMemoryAsync(data);
+      yield return bundleCreateRequest;
+
+      var assetBundle = bundleCreateRequest.assetBundle;
+      if(dontUnload)
+        assetBundle.hideFlags |= HideFlags.DontUnloadUnusedAsset;
+
+      callback(assetBundle);
+      yield break;
+    }
+  }
+}
