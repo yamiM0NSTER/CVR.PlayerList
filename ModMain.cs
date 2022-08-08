9e7e92cd880f7a466dc05431ea83eb1b464431a2
yamiM0NSTER <jomex50@gmail.com>
2022-08-08 11:57:19 +0200

    Execute Patches on Startup

--- ModMain.cs
+++ ModMain.cs
@@ -1,15 +1,20 @@
 ﻿using MelonLoader;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CVR.PlayerListMod
-{
+namespace CVR.PlayerListMod {
   public class ModMain : MelonMod {
+
+    public static ModMain Instance { get; private set; }
+
+    public MelonLogger.Instance Logger => Instance.LoggerInstance;
+
     public override void OnApplicationStart() {
+      Instance = this;
       LoggerInstance.Msg("Hello");
+      Patches.CVR_MenuManager.Patch();
     }
   }
 }
