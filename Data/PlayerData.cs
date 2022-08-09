a709c5a0d76fd288f2d392979ae115f06a29c4ab
yamiM0NSTER <jomex50@gmail.com>
2022-08-09 16:43:46 +0200

    Add PlayerData class

---
+++ Data/PlayerData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CVR.PlayerListMod.Data {
+  internal class PlayerData {
+    string _id;
+    string _name;
+
+    public string Name => _name;
+    public string Id => _id;
+
+    public PlayerData(string id, string name) {
+      _id = id;
+      _name = name;
+    }
+  }
+}
